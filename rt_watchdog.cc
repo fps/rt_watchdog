03fdb4b06e088b3931b007463b59ac70fcd1aa17
FPS <mista.tapas@gmx.net>
2024-07-02 14:59:36 +0200

    - working on it

---
+++ rt_watchdog.cc
@@ -0,0 +1,86 @@
+#include <boost/program_options.hpp>
+
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#include <iostream>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+timespec waker_timeout{5, 0};
+
+void *waker(void *)
+{
+  while(true)
+  {
+    if (-1 == nanosleep(&waker_timeout, 0))
+    {
+      std::cout << "Failed to sleep: " << strerror(errno) << ". Exiting.\n";
+      exit(1);
+    }
+
+    std::cout << "Waking.\n";
+
+    if (0 != pthread_cond_signal(&cond))
+    {
+      std::cout << "Failed to signal condition variable. Exiting.\n";
+      exit(1);
+    }
+  }
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  pthread_t waker_thread;
+  if (0 != pthread_create(&waker_thread, 0, waker, 0))
+  {
+    std::cout << "Failed to create waker thread. Exiting.\n";
+    exit(1);
+  }
+
+  bool first_wakeup = true;
+  timespec last_wakeup;
+  while(true)
+  {
+    if (0 != pthread_mutex_lock(&mutex))
+    {
+      std::cout << "Failed to lock mutex. Exiting.\n";
+      exit(1);
+    }
+
+    timeval current_time;
+    if (0 != gettimeofday(&current_time, 0))
+    {
+      std::cout << "Failed to get current time. Exiting.\n";
+      exit(1);
+    }
+
+    timespec timeout{current_time.tv_sec + 1, current_time.tv_usec * 1000};
+    int wait_res = pthread_cond_timedwait(&cond, &mutex, &timeout);
+    switch(wait_res)
+    {
+      case 0:
+        std::cout << "Woken.\n";
+        break;
+      case ETIMEDOUT:
+        std::cout << "Timeout.\n";
+        break;
+      default:
+        std::cout << "Failed to wait on condition variable. Exiting.\n";
+        exit(1);
+    }
+
+    if (0 != pthread_mutex_unlock(&mutex))
+    {
+      std::cout << "Failed to unlock mutex. Exiting.\n";
+      exit(1);
+    }
+  }
+
+  exit(0);
+}
--- test.sh
+++
@@ -1,6 +0,0 @@
-echo $$
-
-if [ "$1" = "foo" -a "$2" = "bar" -a "$3" = "yippie" ]
-then
-	echo foobaryipi
-fi
